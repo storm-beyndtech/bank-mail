5ea7bf2d87d1c4cc3da5b89575b6bd9dad4c4315
storm <khameleonstorm@gmail.com>
2025-03-21 15:54:58 +0100

    building

--- dist/services/emailService.js
+++ dist/services/emailService.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.bulkMail = bulkMail;
 const emailTemplate_1 = require("./emailTemplate");
@@ -17,10 +26,10 @@ const sendMail = (mailData) => {
         });
     });
 };
-const sendMailWithRetry = async (mailData, retries = 3) => {
+const sendMailWithRetry = (mailData_1, ...args_1) => __awaiter(void 0, [mailData_1, ...args_1], void 0, function* (mailData, retries = 3) {
     for (let i = 0; i < retries; i++) {
         try {
-            return await sendMail(mailData);
+            return yield sendMail(mailData);
         }
         catch (error) {
             if (i === retries - 1)
@@ -28,11 +37,12 @@ const sendMailWithRetry = async (mailData, retries = 3) => {
             console.log(`Retrying sendMail... Attempt ${i + 1}`);
         }
     }
-};
+});
 // Verification Code Mail
-async function bulkMail(mails, subject, message) {
-    try {
-        let bodyContent = `
+function bulkMail(mails, subject, message) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            let bodyContent = `
       <td style="padding: 20px; line-height: 1.5;">
         <h2 style="font-size: 24px;">Hello </h2>
         <p>${message}</p>
@@ -40,15 +50,16 @@ async function bulkMail(mails, subject, message) {
         <p>The Instantsglobal Team</p>
       </td>
     `;
-        let mailOptions = {
-            from: `Instantsglobal <support@mirrorexp.com>`,
-            to: mails,
-            subject,
-            html: (0, emailTemplate_1.emailTemplate)(subject, bodyContent),
-        };
-        return await sendMailWithRetry(mailOptions);
-    }
-    catch (error) {
-        return { error: error instanceof Error && error.message };
-    }
+            let mailOptions = {
+                from: `Instantsglobal <support@mirrorexp.com>`,
+                to: mails,
+                subject,
+                html: (0, emailTemplate_1.emailTemplate)(subject, bodyContent),
+            };
+            return yield sendMailWithRetry(mailOptions);
+        }
+        catch (error) {
+            return { error: error instanceof Error && error.message };
+        }
+    });
 }
